c4b408ae9544d95ec800f771f0d850782bae50b5
olekfur <olekfur@gmail.com>
2020-08-15 18:23:08 +0200

    add some auth handling in navbar and app

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 import Navigation from '../Navigation';
@@ -11,24 +11,48 @@ import Account from '../Account';
 import Admin from '../Admin';
 
 import * as ROUTES from '../../constants/routes';
+import { withFirebase } from '../Firebase';
 //This component is the container to which all fixed components are going (e.g. navbar, sidebar etc.)
 
-const App = () => (
-    <Router>
-        <div>
-            <Navigation />
+class App extends Component {
+    constructor(props) {
+        super(props);
 
-            <hr />
+        this.state = {
+            authUser: null,
+        };
+    }
 
-            <Route exact path={ROUTES.LANDING} component={Landing} />
-            <Route exact path={ROUTES.SIGN_UP} component={SignUp} />
-            <Route exact path={ROUTES.SIGN_IN} component={SignIn} />
-            <Route exact path={ROUTES.PASSWORD_FORGET} component={PasswordForget} />
-            <Route exact path={ROUTES.HOME} component={Home} />
-            <Route exact path={ROUTES.ACCOUNT} component={Account} />
-            <Route exact path={ROUTES.ADMIN} component={Admin} />
-        </div>
-    </Router>
-);
+    componentDidMount() {
+        this.listener = this.props.firebase.auth.onAuthStateChanged(authUser => {
+            authUser
+                ? this.setState({ authUser })
+                : this.setState({ authUser: null });
+        })
+    }
 
-export default App;
+    componentWillUnmount() {
+        this.listener();
+    }
+
+    render() {
+        return (
+            <Router>
+                <div>
+                    <Navigation authUser={this.state.authUser} />
+
+                    <hr/>
+                    <Route exact path={ROUTES.LANDING} component={Landing} />
+                    <Route exact path={ROUTES.SIGN_UP} component={SignUp} />
+                    <Route exact path={ROUTES.SIGN_IN} component={SignIn} />
+                    <Route exact path={ROUTES.PASSWORD_FORGET} component={PasswordForget} />
+                    <Route exact path={ROUTES.HOME} component={Home} />
+                    <Route exact path={ROUTES.ACCOUNT} component={Account} />
+                    <Route exact path={ROUTES.ADMIN} component={Admin} />
+                </div>
+            </Router>
+        );
+    }
+}
+
+export default withFirebase(App);
