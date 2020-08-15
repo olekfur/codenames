8a25d1e44465a399c1c13967d18da4837f6e3d9f
olekfur <olekfur@gmail.com>
2020-08-15 13:32:01 +0200

    add routing framework

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+import Navigation from '../Navigation';
+import Landing from '../Landing';
+import SignUp from '../SignUp';
+import SignIn from '../SignIn';
+import PasswordForget from '../PasswordForget';
+import Home from '../Home';
+import Account from '../Account';
+import Admin from '../Admin';
+
+import * as ROUTES from '../../constants/routes';
+//This component is the container to which all fixed components are going (e.g. navbar, sidebar etc.)
 
 const App = () => (
-    <div>
-        <h1>Appindex</h1>
-    </div>
+    <Router>
+        <div>
+            <Navigation />
+
+            <hr />
+
+            <Route exact path={ROUTES.LANDING} component={Landing} />
+            <Route exact path={ROUTES.SIGN_UP} component={SignUp} />
+            <Route exact path={ROUTES.SIGN_IN} component={SignIn} />
+            <Route exact path={ROUTES.PASSWORD_FORGET} component={PasswordForget} />
+            <Route exact path={ROUTES.HOME} component={Home} />
+            <Route exact path={ROUTES.ACCOUNT} component={Account} />
+            <Route exact path={ROUTES.ADMIN} component={Admin} />
+        </div>
+    </Router>
 );
 
 export default App;
